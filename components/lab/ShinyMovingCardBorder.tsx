07eb9400950e0e023204e5563a99da8e953da3f0
ibelick <julien.thibeaut@gmail.com>
2023-05-02 16:36:11 +0200

    feat: add border tw, clean text

--- components/lab/ShinyMovingCardBorder.tsx
+++ components/lab/ShinyMovingCardBorder.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from "next-themes";
-import React, { useEffect } from "react";
+import React, { CSSProperties, useEffect, useRef } from "react";
 
 export const ShinyMovingCardBorderTheme = () => {
   const { theme, setTheme } = useTheme();
@@ -79,12 +79,7 @@ export const ShinyMovingCardBorderTheme = () => {
   );
 };
 
-type ShinyMovingCardBorderProps = {
-  children: React.ReactNode;
-  borderColor: string;
-};
-
-export const ShinyMovingCardBorder: React.FC<ShinyMovingCardBorderProps> = ({
+export const ShinyMovingCardBorder: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
@@ -120,5 +115,44 @@ export const ShinyMovingCardBorder: React.FC<ShinyMovingCardBorderProps> = ({
         `}
       </style>
     </>
+  );
+};
+
+export const ShinyMovingCardBorderTW: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const boxRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const boxElement = boxRef.current;
+
+    if (!boxElement) {
+      return;
+    }
+
+    const updateAnimation = () => {
+      const angle =
+        (parseFloat(boxElement.style.getPropertyValue("--angle")) + 0.5) % 360;
+      boxElement.style.setProperty("--angle", `${angle}deg`);
+      requestAnimationFrame(updateAnimation);
+    };
+
+    requestAnimationFrame(updateAnimation);
+  }, []);
+
+  return (
+    <div
+      ref={boxRef}
+      style={
+        {
+          "--angle": "0deg",
+          "--border-color": "linear-gradient(var(--angle), #070707, #687aff)",
+          "--bg-color": "linear-gradient(#131219, #131219)",
+        } as CSSProperties
+      }
+      className="flex h-[400px] w-[400px] items-center justify-center rounded-lg border-2 border-[#0000] p-3 [background:padding-box_var(--bg-color),border-box_var(--border-color)]"
+    >
+      {children}
+    </div>
   );
 };
