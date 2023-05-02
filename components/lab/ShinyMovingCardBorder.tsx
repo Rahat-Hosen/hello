5369de92487dae6aa5c186d37af58b73403f4ee1
ibelick <julien.thibeaut@gmail.com>
2023-05-02 14:34:21 +0200

    feat: update next-themes, tailwindcss, add article shiny moving border

--- components/lab/ShinyMovingCardBorder.tsx
+++ components/lab/ShinyMovingCardBorder.tsx
@@ -1,8 +1,8 @@
 import { useTheme } from "next-themes";
 import React, { useEffect } from "react";
 
-export const ShinyMovingCardBorder = () => {
-  const { theme } = useTheme();
+export const ShinyMovingCardBorderTheme = () => {
+  const { theme, setTheme } = useTheme();
   const [background, setBackground] = React.useState<string | null>(null);
   const [border, setBorder] = React.useState<string | null>(null);
   const [boxShadow, setBoxShadow] = React.useState<string | null>(null);
@@ -34,17 +34,76 @@ export const ShinyMovingCardBorder = () => {
 
   return (
     <>
-      <div className="box" />
+      <div
+        className="box"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      >
+        <span>Trigger theme</span>
+      </div>
       <style jsx>
         {`
           .box {
+            user-select: none;
+            cursor: pointer;
+            display: flex;
+            justify-content: center;
+            align-items: center;
             height: 200px;
             width: 200px;
-            border: 3px solid #0000;
+            border: 1px solid #0000;
             border-radius: 12px;
             background: ${background}, ${border};
-            animation: 4s rotate linear infinite;
+            animation: 5s rotate linear infinite;
             box-shadow: ${boxShadow};
+          }
+
+          .box:active > span {
+            transition: 0.1s scale ease-in-out;
+            scale: 0.95;
+          }
+
+          @keyframes rotate {
+            to {
+              --angle: 360deg;
+            }
+          }
+
+          @property --angle {
+            syntax: "<angle>";
+            initial-value: 0deg;
+            inherits: false;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+type ShinyMovingCardBorderProps = {
+  children: React.ReactNode;
+  borderColor: string;
+};
+
+export const ShinyMovingCardBorder: React.FC<ShinyMovingCardBorderProps> = ({
+  children,
+}) => {
+  return (
+    <>
+      <div className="box">{children}</div>
+      <style jsx>
+        {`
+          .box {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 12px;
+            height: 400px;
+            width: 400px;
+            border: 1px solid #0000;
+            border-radius: 12px;
+            background: linear-gradient(#06021d, #06021d) padding-box,
+              linear-gradient(var(--angle), #070707, #687aff) border-box;
+            animation: 8s rotate linear infinite;
           }
 
           @keyframes rotate {
