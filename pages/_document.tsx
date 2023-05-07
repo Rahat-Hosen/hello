c63f553fa0c070d409eb40f8e600e6f8e3bb6cb1
ibelick <julien.thibeaut@gmail.com>
2023-05-07 18:42:57 +0200

    feat: add unami script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,11 @@ class MyDocument extends Document {
             rel="shortcut icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⛵️</text></svg>"
           />
+          <script
+            async
+            src="https://analytics.umami.is/script.js"
+            data-website-id="6e431bc3-cebc-4206-a5d2-bd963aab57a8"
+          ></script>
         </Head>
         <body>
           <Main />
