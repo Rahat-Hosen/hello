5cf0e89c2f935afd5fbc178789ecce6ddc501c6f
ibelick <julien.thibeaut@gmail.com>
2023-05-09 22:54:32 +0200

    feat: remove analytics if dev

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,8 @@ class MyDocument extends Document {
   }
 
   render() {
+    const isDev = process.env.NODE_ENV === "development";
+
     return (
       <Html lang="en">
         <Head>
@@ -20,11 +22,13 @@ class MyDocument extends Document {
             rel="shortcut icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⛵️</text></svg>"
           />
-          <script
-            async
-            src="https://analytics.umami.is/script.js"
-            data-website-id="6e431bc3-cebc-4206-a5d2-bd963aab57a8"
-          ></script>
+          {!isDev ? (
+            <script
+              async
+              src="https://analytics.umami.is/script.js"
+              data-website-id="6e431bc3-cebc-4206-a5d2-bd963aab57a8"
+            ></script>
+          ) : null}
         </Head>
         <body>
           <Main />
