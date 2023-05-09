d7d7361c317f76e58c52e8efd4dc1620cdb9225d
ibelick <julien.thibeaut@gmail.com>
2023-05-09 12:40:30 +0200

    feat: add spotlight effect article, components

--- components/ComponentPlayground.tsx
+++ components/ComponentPlayground.tsx
@@ -4,7 +4,7 @@ const ComponentPlayground: React.FC<{
 }> = ({ children, className }) => {
   return (
     <div
-      className={`flex items-center justify-center rounded-xl border border-slate-600/50 bg-slate-950 px-4 py-12 ${className}`}
+      className={`not-prose flex items-center justify-center rounded-xl border border-slate-600/50 bg-slate-950 px-4 py-12 ${className}`}
     >
       {children}
     </div>
