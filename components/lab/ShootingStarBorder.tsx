716e23b8657762fa5e3b0258e20f07dcdfb2d45a
ibelick <julien.thibeaut@gmail.com>
2023-05-15 15:24:07 +0200

    feat: add article shooting star border

--- components/lab/ShootingStarBorder.tsx
+++ components/lab/ShootingStarBorder.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
 export const CardShootingStarBorderCSS = () => {
   return (
@@ -8,61 +8,36 @@ export const CardShootingStarBorderCSS = () => {
           <span className="spark" />
         </span>
         <span className="backdrop" />
-        <span className="text">Shim Shimmer</span>
+        <span className="text">Shooting star border</span>
       </button>
       <style jsx>{`
         button {
           --transition: 0.25s;
-          --spark: 1.8s;
-          --speed: 1.2s;
-          --cut: 0.1em;
-          --active: 0;
-          --bg: radial-gradient(
-              40% 50% at center 100%,
-              hsl(270 0% 72% / 0.05),
-              transparent
-            ),
-            radial-gradient(
-              80% 100% at center 120%,
-              hsl(260 0% 70% / 0.1),
-              transparent
-            ),
-            hsl(260 0% 12%);
+          --spark: 3s;
+          --bg: black;
           background: var(--bg);
-          font-size: 2rem;
-          font-weight: 500;
-          border: 0;
-          cursor: pointer;
-          padding: 0.9em 1.3em;
+          padding: 6px 18px;
           display: grid;
-          place-items: center;
-          white-space: nowrap;
-          border-radius: 100px;
+          border-radius: 9999px;
           position: relative;
           overflow: hidden;
-          box-shadow: 0 0.05em 0 0 hsl(260 0% 50%) inset,
-            0 -0.05em 0 0 hsl(260 0% 0%) inset;
-          transition: box-shadow var(--transition), background var(--transition);
+          box-shadow: 0 1000px 0 0 hsl(0 0% 20%) inset;
+          transition: box-shadow var(--transition), background var(--transition),
+            transform var(--transition);
         }
 
-        label {
-          color: hsl(0 0% 98%);
-          position: absolute;
-          top: 4rem;
-          left: 1rem;
+        button:hover .backdrop {
+          background: rgb(20 20 20);
         }
-        [type="checkbox"] {
-          position: absolute;
-          top: 4rem;
-          left: 7rem;
-          translate: 0 25%;
-          accent-color: hsl(280 80% 50%);
+
+        button:hover {
+          transform: scale(1.05);
         }
 
         .spark {
           position: absolute;
           inset: 0;
-          border-radius: 100px;
+          border-radius: 9999px;
           rotate: 0deg;
           overflow: hidden;
           mask: linear-gradient(white, transparent 50%);
@@ -85,7 +60,6 @@ export const CardShootingStarBorderCSS = () => {
           translate: -50% -15%;
           rotate: 0;
           transform: rotate(-90deg);
-          opacity: calc((var(--active)) + 0.4);
           opacity: 1;
           background: conic-gradient(
             from 0deg,
@@ -96,18 +70,11 @@ export const CardShootingStarBorderCSS = () => {
           animation: rotate var(--spark) linear infinite both;
         }
 
-        .spark:after {
-          content: "";
-          position: absolute;
-          inset: var(--cut);
-          border-radius: 100px;
-        }
-
         .backdrop {
           position: absolute;
-          inset: var(--cut);
+          inset: 1px;
           background: var(--bg);
-          border-radius: 100px;
+          border-radius: 9999px;
           transition: background var(--transition) opacity var(--transition);
         }
 
@@ -117,19 +84,25 @@ export const CardShootingStarBorderCSS = () => {
           }
         }
 
-        button:is(:hover, :focus-visible) ~ :is(.bodydrop, .particle-pen) {
-          --active: 1;
-        }
-
-        button:is(:hover, :focus-visible) {
-          --active: 1;
-        }
-
         .text {
           z-index: 1;
           color: rgb(203 213 225);
         }
       `}</style>
+    </>
+  );
+};
+
+export const ShootingStarBorderTW = () => {
+  return (
+    <>
+      <button className="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
+        <span>
+          <span className="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
+        </span>
+        <span className="backdrop absolute inset-[1px] rounded-full bg-black transition-colors duration-200 group-hover:bg-slate-800" />
+        <span className="text z-10 text-[#cbd5e1]">Shooting star border</span>
+      </button>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,22 @@ module.exports = {
           },
         };
       },
+      animation: {
+        flip: "flip 6s infinite steps(2, end)",
+        rotate: "rotate 3s linear infinite both",
+      },
+      keyframes: {
+        flip: {
+          to: {
+            transform: "rotate(360deg)",
+          },
+        },
+        rotate: {
+          to: {
+            transform: "rotate(90deg)",
+          },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
