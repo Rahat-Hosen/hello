b187f82a61e52d01e06a5c7ba1b1c50712220272
ibelick <julien.thibeaut@gmail.com>
2023-06-01 14:48:20 +0200

    feat: quick update select projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,167 +50,176 @@ export default function Home() {
           personal website!
         </p>
       </div>
-      <h2 className="mt-12 font-medium">Selected projects</h2>
-      <ul className="items flex max-w-xs list-none flex-col justify-between divide-y">
-        <li className="m-0 mt-5 py-2 pl-0">
-          <AccordionProject
-            title="basyl"
-            date={2023}
-            description="An AI cooking assistant. Experiment with Open AI's GPT-3 to generate recipes and cooking instructions."
-            links={[
-              {
-                text: "Website",
-                link: "https://basyl.co/",
-              },
-            ]}
-          />
-        </li>
-        <li className="m-0 py-2 pl-0">
-          <AccordionProject
-            title="rapsodie"
-            date={2022}
-            description="A mobile game where you manage a rap music label. Developed the beta version of the app with React and Capacitor."
-            links={[
-              {
-                text: "Website",
-                link: "https://rapsodie.co/",
-              },
-              {
-                text: "App Store",
-                link: "https://apps.apple.com/fr/app/rapsodie-ton-label-de-rap/id1572281230",
-              },
-              {
-                text: "Play Store",
-                link: "https://play.google.com/store/apps/details?id=com.rapsodiedata.app&hl=fr&gl=US",
-              },
-            ]}
-          />
-        </li>
-        <li className="m-0 py-2 pl-0">
-          <AccordionProject
-            title="newfrenz"
-            date={2022}
-            description="Onboarding app for web3 concepts. Won the 1st prize of OpenSea
+      <div className="prose mt-24 dark:prose-dark prose-ul:pl-0">
+        <h2 className="mt-12 text-base font-medium">Selected projects</h2>
+        <p>
+          Below is a showcase of various projects I've worked on over the years.
+          These include both client work and personal projects. Please note that
+          while I'm proud of all these projects, some of them are no longer
+          actively maintained. Feel free to click on any project to expand and
+          learn more about it.
+        </p>
+        <ul className="items flex max-w-xs list-none flex-col justify-between divide-y">
+          <li className="m-0 mt-0 py-2 pl-0">
+            <AccordionProject
+              title="basyl"
+              date={2023}
+              description="An AI cooking assistant. Experiment with Open AI's GPT-3 to generate recipes and cooking instructions."
+              links={[
+                {
+                  text: "Website",
+                  link: "https://basyl.co/",
+                },
+              ]}
+            />
+          </li>
+          <li className="m-0 py-2 pl-0">
+            <AccordionProject
+              title="rapsodie"
+              date={2022}
+              description="A mobile game where you manage a rap music label. Developed the beta version of the app with React and Capacitor."
+              links={[
+                {
+                  text: "Website",
+                  link: "https://rapsodie.co/",
+                },
+                {
+                  text: "App Store",
+                  link: "https://apps.apple.com/fr/app/rapsodie-ton-label-de-rap/id1572281230",
+                },
+                {
+                  text: "Play Store",
+                  link: "https://play.google.com/store/apps/details?id=com.rapsodiedata.app&hl=fr&gl=US",
+                },
+              ]}
+            />
+          </li>
+          <li className="m-0 py-2 pl-0">
+            <AccordionProject
+              title="newfrenz"
+              date={2022}
+              description="Onboarding app for web3 concepts. Won the 1st prize of OpenSea
                 x Replit Hackathon with it."
-            links={[
-              {
-                text: "Website",
-                link: "https://www.newfrenz.xyz/",
-              },
-            ]}
-          />
-        </li>
-        <li className="m-0 py-2 pl-0">
-          <AccordionProject
-            title="hellocurator"
-            date={2022}
-            description="Concept app to explore digital art curation. Worked on front-end and product. Built with Next.js and web3 technologies."
-            links={[
-              {
-                text: "Website",
-                link: "https://hellocurator.xyz/",
-              },
-              {
-                text: "Article",
-                link: "https://mirror.xyz/hellocurator.eth/ejbQtqckavjt1aS7kScgXq8qkIMobov0kbT9P2eW9FE",
-              },
-            ]}
-          />
-        </li>
-        <li className="m-0 py-2 pl-0">
-          <AccordionProject
-            title="playgrounds"
-            date={2022}
-            description="Community of builders who experiment building decentralized
+              links={[
+                {
+                  text: "Website",
+                  link: "https://www.newfrenz.xyz/",
+                },
+              ]}
+            />
+          </li>
+          <li className="m-0 py-2 pl-0">
+            <AccordionProject
+              title="hellocurator"
+              date={2022}
+              description="Concept app to explore digital art curation. Worked on front-end and product. Built with Next.js and web3 technologies."
+              links={[
+                {
+                  text: "Website",
+                  link: "https://hellocurator.xyz/",
+                },
+                {
+                  text: "Article",
+                  link: "https://mirror.xyz/hellocurator.eth/ejbQtqckavjt1aS7kScgXq8qkIMobov0kbT9P2eW9FE",
+                },
+              ]}
+            />
+          </li>
+          <li className="m-0 py-2 pl-0">
+            <AccordionProject
+              title="playgrounds"
+              date={2022}
+              description="Community of builders who experiment building decentralized
                 applications."
-            links={[
-              {
-                text: "Website",
-                link: "https://playgrounds.wtf/",
-              },
-            ]}
-          />
-        </li>
-        <li className="m-0 py-2 pl-0">
-          <AccordionProject
-            title="swile"
-            date={2021}
-            description="Helped building components for the new Swile website."
-            links={[
-              {
-                text: "Website",
-                link: "https://swile.co/",
-              },
-            ]}
-          />
-        </li>
-        <li className="m-0 py-2 pl-0">
-          <AccordionProject
-            title="uxcademy"
-            date={2021}
-            description="A website to learn UI/UX design through interactive quizzes. +1500 actives learners. Worked on the front-end and product. Built with Next.js"
-            links={[
-              {
-                text: "Website",
-                link: "https://uxcademy.com/",
-              },
-            ]}
-          />
-        </li>
-        <li className="m-0 py-2 pl-0">
-          <AccordionProject
-            title="lafourche"
-            date={`2020-21`}
-            description="Best online shop to buy organic food in France. Leading the front-end development, from Shopify to Next.js. Front-end architecture & implementation of the new branding"
-            links={[
-              {
-                text: "Website",
-                link: "https://lafourche.fr/",
-              },
-            ]}
-          />
-        </li>
-        <li className="m-0 py-2 pl-0">
-          <AccordionProject
-            title="madely"
-            date={2019}
-            description="Parisian video based event discovery app (available on iOS). Worked on front-end and product. Built with React Native and Expo."
-            links={[
-              {
-                text: "Dribbble",
-                link: "https://dribbble.com/shots/9683319-Madely-Event-Discovery-App",
-              },
-            ]}
-          />
-        </li>
-        <li className="m-0 py-2 pl-0">
-          <AccordionProject
-            title="Foncia R&D"
-            date={`2018-19`}
-            description="Worked on front-end development for the R&D team. Simplification of the product for one of the biggest real estate agency in France."
-            links={[
-              {
-                text: "Website",
-                link: "https://fr.foncia.com/",
-              },
-            ]}
-          />
-        </li>
-        <li className="m-0 py-2 pl-0">
-          <AccordionProject
-            title="bitcallz"
-            date={2017}
-            description="Crypto-prediction platform. Worked on fullstack development and product. Built with React and Node.js."
-            links={[
-              {
-                text: "Dribbble",
-                link: "https://dribbble.com/shots/9390744-Bitcallz-crypto-prediction-platform",
-              },
-            ]}
-          />
-        </li>
-      </ul>
-      <div className="mt-12">
+              links={[
+                {
+                  text: "Website",
+                  link: "https://playgrounds.wtf/",
+                },
+              ]}
+            />
+          </li>
+          <li className="m-0 py-2 pl-0">
+            <AccordionProject
+              title="swile"
+              date={2021}
+              description="Helped building components for the new Swile website."
+              links={[
+                {
+                  text: "Website",
+                  link: "https://swile.co/",
+                },
+              ]}
+            />
+          </li>
+          <li className="m-0 py-2 pl-0">
+            <AccordionProject
+              title="uxcademy"
+              date={2021}
+              description="A website to learn UI/UX design through interactive quizzes. +1500 actives learners. Worked on the front-end and product. Built with Next.js"
+              links={[
+                {
+                  text: "Website",
+                  link: "https://uxcademy.com/",
+                },
+              ]}
+            />
+          </li>
+          <li className="m-0 py-2 pl-0">
+            <AccordionProject
+              title="lafourche"
+              date={`2020-21`}
+              description="Best online shop to buy organic food in France. Leading the front-end development, from Shopify to Next.js. Front-end architecture & implementation of the new branding"
+              links={[
+                {
+                  text: "Website",
+                  link: "https://lafourche.fr/",
+                },
+              ]}
+            />
+          </li>
+          <li className="m-0 py-2 pl-0">
+            <AccordionProject
+              title="madely"
+              date={2019}
+              description="Parisian video based event discovery app (available on iOS). Worked on front-end and product. Built with React Native and Expo."
+              links={[
+                {
+                  text: "Dribbble",
+                  link: "https://dribbble.com/shots/9683319-Madely-Event-Discovery-App",
+                },
+              ]}
+            />
+          </li>
+          <li className="m-0 py-2 pl-0">
+            <AccordionProject
+              title="Foncia R&D"
+              date={`2018-19`}
+              description="Worked on front-end development for the R&D team. Simplification of the product for one of the biggest real estate agency in France."
+              links={[
+                {
+                  text: "Website",
+                  link: "https://fr.foncia.com/",
+                },
+              ]}
+            />
+          </li>
+          <li className="m-0 py-2 pl-0">
+            <AccordionProject
+              title="bitcallz"
+              date={2017}
+              description="Crypto-prediction platform. Worked on fullstack development and product. Built with React and Node.js."
+              links={[
+                {
+                  text: "Dribbble",
+                  link: "https://dribbble.com/shots/9390744-Bitcallz-crypto-prediction-platform",
+                },
+              ]}
+            />
+          </li>
+        </ul>
+      </div>
+      <div className="mt-24">
         <SectionContact />
       </div>
     </div>
