bda4626190ad0688c4e0b8380f84648d1cd9407d
ibelick <julien.thibeaut@gmail.com>
2023-06-03 00:47:08 +0200

    feat: init tilt effect article

---
+++ components/lab/TiltEffect.tsx
@@ -0,0 +1,38 @@
+import { useState, MouseEvent } from "react";
+
+export const TiltEffect = () => {
+  const [rotate, setRotate] = useState({ x: 0, y: 0 });
+
+  const onMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    const card = e.currentTarget;
+    const box = card.getBoundingClientRect();
+    const x = e.clientX - box.left;
+    const y = e.clientY - box.top;
+    const centerX = box.width / 2;
+    const centerY = box.height / 2;
+    const rotateX = (y - centerY) / 10;
+    const rotateY = (centerX - x) / 10;
+
+    setRotate({ x: rotateX, y: rotateY });
+  };
+
+  const onMouseLeave = () => {
+    setRotate({ x: 0, y: 0 });
+  };
+
+  return (
+    <>
+      <div
+        className="card h-52 w-52 bg-white transition-[all_400ms_cubic-bezier(0.03,0.98,0.52,0.99)_0s]"
+        onMouseMove={onMouseMove}
+        onMouseLeave={onMouseLeave}
+        style={{
+          transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${rotate.y}deg) scale3d(1, 1, 1)`,
+          transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+        }}
+      >
+        {" "}
+      </div>
+    </>
+  );
+};
