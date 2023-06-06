e77ac924c9cf3ef4d2b53e1916f80ce0934fcff9
ibelick <julien.thibeaut@gmail.com>
2023-06-06 16:09:14 +0200

    feat: add tilt effect article

--- components/lab/TiltEffect.tsx
+++ components/lab/TiltEffect.tsx
@@ -1,20 +1,38 @@
-import { useState, MouseEvent } from "react";
+import { useState, MouseEvent, useCallback } from "react";
+
+function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  let lastCall = 0;
+  return (...args: Parameters<T>) => {
+    const now = new Date().getTime();
+    if (now - lastCall < delay) {
+      return;
+    }
+    lastCall = now;
+    return func(...args);
+  };
+}
 
 export const TiltEffect = () => {
   const [rotate, setRotate] = useState({ x: 0, y: 0 });
 
-  const onMouseMove = (e: MouseEvent<HTMLDivElement>) => {
-    const card = e.currentTarget;
-    const box = card.getBoundingClientRect();
-    const x = e.clientX - box.left;
-    const y = e.clientY - box.top;
-    const centerX = box.width / 2;
-    const centerY = box.height / 2;
-    const rotateX = (y - centerY) / 10;
-    const rotateY = (centerX - x) / 10;
+  const onMouseMove = useCallback(
+    throttle((e: MouseEvent<HTMLDivElement>) => {
+      const card = e.currentTarget;
+      const box = card.getBoundingClientRect();
+      const x = e.clientX - box.left;
+      const y = e.clientY - box.top;
+      const centerX = box.width / 2;
+      const centerY = box.height / 2;
+      const rotateX = (y - centerY) / 7;
+      const rotateY = (centerX - x) / 7;
 
-    setRotate({ x: rotateX, y: rotateY });
-  };
+      setRotate({ x: rotateX, y: rotateY });
+    }, 100),
+    []
+  );
 
   const onMouseLeave = () => {
     setRotate({ x: 0, y: 0 });
@@ -23,7 +41,7 @@ export const TiltEffect = () => {
   return (
     <>
       <div
-        className="card h-52 w-52 bg-white transition-[all_400ms_cubic-bezier(0.03,0.98,0.52,0.99)_0s]"
+        className="card relative h-52 w-52 rounded-xl bg-white transition-[all_400ms_cubic-bezier(0.03,0.98,0.52,0.99)_0s] will-change-transform"
         onMouseMove={onMouseMove}
         onMouseLeave={onMouseLeave}
         style={{
@@ -31,7 +49,10 @@ export const TiltEffect = () => {
           transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
         }}
       >
-        {" "}
+        <div className="pulse absolute -inset-2 rounded-lg bg-gradient-to-r from-blue-600 via-purple-600 to-indigo-600 opacity-75 blur-xl" />
+        <div className="relative flex h-full w-full select-none items-center justify-center rounded-lg bg-slate-900 text-sm font-light text-slate-300">
+          tilt effect
+        </div>
       </div>
     </>
   );
