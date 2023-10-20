e3351a3c460fff7e378a43b7ad8e16e8ec112848
ibelick <julien.thibeaut@gmail.com>
2023-10-20 13:20:52 +0200

    feat: add copy code

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,4 +1,6 @@
 import { Highlight, PrismTheme } from "prism-react-renderer";
+import { CopyIcon, CheckIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 const theme: PrismTheme = {
   plain: {
@@ -101,28 +103,63 @@ const theme: PrismTheme = {
   ],
 };
 
+type CopyCode = {
+  code: string;
+};
+
+const CopyCode: React.FC<CopyCode> = ({ code }) => {
+  const [hasCheckIcon, setHasCheckIcon] = useState(false);
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(code);
+    setHasCheckIcon(true);
+
+    setTimeout(() => {
+      setHasCheckIcon(false);
+    }, 1000);
+  };
+
+  return (
+    <button
+      className="cursor-pointer rounded-md border border-neutral-900 bg-neutral-800 p-1.5 transition hover:bg-neutral-700"
+      onClick={onCopy}
+    >
+      {hasCheckIcon ? (
+        <CheckIcon className="h-3 w-3 text-neutral-50" />
+      ) : (
+        <CopyIcon className="h-3 w-3 text-neutral-50" />
+      )}
+    </button>
+  );
+};
+
 type CodeBlockProps = {
   code: string;
 };
 
 const CodeBlock: React.FC<CodeBlockProps> = ({ code }) => {
   return (
-    <Highlight language="tsx" code={code} theme={theme}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre
-          className={`${className} overflow-auto rounded-[4px] p-4 text-[11px] dark:border dark:border-neutral-800`}
-          style={{ ...style }}
-        >
-          {tokens.slice(0, -1).map((line, i) => (
-            <div {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} />
-              ))}
+    <div>
+      <Highlight language="tsx" code={code} theme={theme}>
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre
+            className={`${className} relative overflow-auto rounded-[4px] p-4 text-[11px] dark:border dark:border-neutral-800`}
+            style={{ ...style }}
+          >
+            <div className="absolute right-3 top-3">
+              <CopyCode code={code} />
             </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
+            {tokens.slice(0, -1).map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    </div>
   );
 };
 
